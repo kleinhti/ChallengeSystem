528f1b5ec3ad904d3701040621c724b10c75c27d
Tim <kleinhans.tim@web.de>
2020-05-29 11:54:33 +0200

    Added Ping command

---
+++ NoahAdrian/src/de/schlauhund/commands/Ping.java
@@ -0,0 +1,24 @@
+package de.schlauhund.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class Ping implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender s, Command arg1, String arg2, String[] args) {
+
+		if (s instanceof Player) {
+			Player p = (Player) s;
+			if (args.length == 0) {
+				int ping = ((CraftPlayer) p).getHandle().ping;
+				p.sendMessage("§ePing: §2" + ping + " ms");
+			}
+		}
+
+		return false;
+	}
+}
